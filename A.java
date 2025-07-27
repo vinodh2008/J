c6bd3493295e5a0b34ddbe8f07df3835d8dd6218
vinodh <yemireddivinodh@gmail.com>
2025-07-27 09:00:39 +0530

    main java file is added

---
+++ A.java
@@ -0,0 +1,9 @@
+class A
+{
+    public static void main(String[] args) {
+        System.err.println("vinodh kumar welcome");
+        for (int i = 0; i < 5; i++) {
+            System.err.println("vinodh");
+        }
+    }
+}
